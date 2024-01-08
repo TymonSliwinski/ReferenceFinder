fcf5a530e10f056b25d860149e29f9e2aff370f6
Tymon Śliwiński <tymon.sliwinski24@gmail.com>
2024-01-08 23:56:52 +0100

    ebeddings: add type annotations

--- embedding_service/embeddings.py
+++ embedding_service/embeddings.py
@@ -4,9 +4,10 @@ from sentence_transformers import SentenceTransformer
 class Embeddings:
     _device = 'cuda' if torch.cuda.is_available() else 'cpu'
     _model = SentenceTransformer('all-MiniLM-L6-v2', device=_device)
+    dimension = _model.get_sentence_embedding_dimension()
     
     def __init__(self):
         pass
 
-    def embed(self, text):
+    def embed(self, text: str) -> list[float]:
         return self._model.encode(text).tolist()
