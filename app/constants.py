0440dfeb8a17da52d7260ab358f63dae00ece0a1
Tymon Śliwiński <tymon.sliwinski24@gmail.com>
2024-01-09 18:43:12 +0100

    chore: change project structure

--- app/constants.py
+++ app/constants.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-if os.path.exists('../.env'):
-    load_dotenv(dotenv_path='../.env')
+if os.path.exists('.env'):
+    load_dotenv(dotenv_path='.env')
 
 try:
     PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
